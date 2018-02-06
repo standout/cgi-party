84bcff7fa89a90e1353ecf47f5affa5ed16d28fc
Emric <w.e.w@live.se>
2018-02-06 11:13:17 +0100

    fixup! Refactor specs

--- spec/authenticate_response_spec.rb
+++ spec/authenticate_response_spec.rb
@@ -10,9 +10,11 @@ RSpec.describe CGIParty::AuthenticateResponse do
 
     response = CGIParty::AuthenticateResponse.new(response_body)
 
-    expect(response.transaction_id).to eq response_body[:authenticate_response][:transaction_id]
-    expect(response.order_ref).to eq response_body[:authenticate_response][:order_ref]
-    expect(response.auto_start_token).to eq response_body[:authenticate_response][:auto_start_token]
+    expect(response).to have_attributes(
+      auto_start_token: response_body[:authenticate_response][:auto_start_token],
+      transaction_id: response_body[:authenticate_response][:transaction_id],
+      order_ref: response_body[:authenticate_response][:order_ref]
+    )
   end
 
   describe "#autostart_url" do
@@ -20,9 +22,9 @@ RSpec.describe CGIParty::AuthenticateResponse do
     let(:authenticate_response) do
       CGIParty::AuthenticateResponse.new({
         :authenticate_response=> {
-          :transaction_id=>"e7a5c17c",
+          :auto_start_token=>"45216ad0-320a-4de5-b4a3-bac28de5c173",
           :order_ref=>"c87ebe55-0d1d-4d37-a5ed-c0b65f88c4e4",
-          :auto_start_token=>"45216ad0-320a-4de5-b4a3-bac28de5c173"
+          :transaction_id=>"e7a5c17c"
         }
       })
     end
